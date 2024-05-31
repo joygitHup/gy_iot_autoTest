02603d1561383751898c1ccfac7ed94079f862ab
Irven.Lai <Irven.Lai@github.com>
2024-05-31 16:46:08 +0800

    fix main.py question

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from config.path_config import report_path
 case_path1 = os.path.join(os.getcwd(), "test_case")
 def creat_suite():
     suiteTest_defaultTestLoader = unittest.defaultTestLoader.discover(start_dir=case_path1, pattern='test_*.py',
-                                                                      top_level_dir=case_path1)
+                                                                      top_level_dir=None)
     return suiteTest_defaultTestLoader
 suite = creat_suite()
 if __name__ == "__main__":
