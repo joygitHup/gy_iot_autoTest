dcf12a9255d2b36168852853a4a01ef33c5a515c
Irven.Lai <Irven.Lai@github.com>
2024-05-31 18:37:42 +0800

    editor cofig file

--- config/path_config.py
+++ config/path_config.py
@@ -4,10 +4,10 @@ current_directory = os.path.dirname(os.path.abspath(__file__))
 # root_path = os.path.abspath(os.path.dirname(current_directory) + os.path.sep + ".")
 # sys.path.append(root_path)
 report_save_path=os.path.abspath(os.path.join(os.getcwd(), ".."))
-report_path=report_save_path+r'/gy_iot_autotest/results'
-alluredir_path=report_save_path+r'/gy_iot_autotest/report'
-test_case=report_save_path+r'/gy_iot_autotest/test_case'
-SccreShort=report_save_path+r'/gy_iot_autotest/img'
+report_path=report_save_path+r'/gy_iot_autoTest/results'
+alluredir_path=report_save_path+r'/gy_iot_autoTest/report'
+test_case=report_save_path+r'/gy_iot_autoTest/test_case'
+SccreShort=report_save_path+r'/gy_iot_autoTest/img'
 
 print(test_case)
 
